8cce5091c234d6b459a3e7e053311aaeaf8189ff
Brendan Strejcek <brendan@datagazing.com>
2021-06-26 17:45:14 -0400

    Bump version: 1.0.2 → 1.0.3

--- iterstring/__init__.py
+++ iterstring/__init__.py
@@ -73,6 +73,6 @@ Documentation
 
 __author__ = """Brendan Strejcek"""
 __email__ = 'brendan@datagazing.com'
-__version__ = '1.0.2'
+__version__ = '1.0.3'
 
 from .iterstring import Istr, listr, distr, tlist # noqa F401
